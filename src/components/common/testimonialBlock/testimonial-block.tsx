4d3b77e7005235e64a587bc7e02e64f41439e41a
nzkks <kkshanthosh@gmail.com>
2024-08-01 17:03:31 +1200

    Homepage - Recommendations section: populate the content from json data

--- src/components/common/testimonialBlock/testimonial-block.tsx
+++ src/components/common/testimonialBlock/testimonial-block.tsx
@@ -10,8 +10,8 @@ type Props = {
 const TestimonialBlock = ({ imageFileName, name, about, content }: Props) => {
   return (
     <div className="group mx-auto mt-6 w-full p-2 sm:p-4 md:p-8">
-      <figure className="flex flex-col items-center justify-center md:flex-row md:items-start">
-        <div className="relative block">
+      <figure className="grid grid-cols-1 items-center justify-center gap-6 md:grid-cols-[200px_1fr] md:items-start">
+        <div className="relative block md:w-[200px]">
           <Image
             alt={name}
             src={`/images/testimonials/${imageFileName}`}
@@ -20,16 +20,18 @@ const TestimonialBlock = ({ imageFileName, name, about, content }: Props) => {
             className="mx-auto h-40 w-40 rounded-full object-cover grayscale group-hover:grayscale-0"
           />
         </div>
-        <figcaption className="mt-5 w-full md:mt-0 md:w-2/3">
-          <p className="m-auto w-full text-left md:w-2/3">
+        <figcaption className="mt-5 md:mt-0">
+          <div className="m-auto text-left">
             <span className="font-bold">“</span>
-            {content}
+            <div dangerouslySetInnerHTML={{ __html: content }} />
             <span className="font-bold">”</span>
-          </p>
-          <div className="mt-8 flex items-center justify-center">
-            <span className="mr-2 font-semibold">{name}</span>
-            <span className="font-light">/</span>
-            <span className="ml-2">{about}</span>
+          </div>
+          <div className="mt-8">
+            <span className="flex flex-col md:inline-flex md:flex-row">
+              <span className="font-semibold md:mr-2">{name}</span>
+              <span className="hidden font-light md:inline">/</span>
+              <span className="md:ml-2">{about}</span>
+            </span>
           </div>
         </figcaption>
       </figure>
--- src/components/homepage/recommendations/recommendations.tsx
+++ src/components/homepage/recommendations/recommendations.tsx
@@ -1,17 +1,21 @@
 import { Container, TestimonialBlock } from '@/components';
+import recommendationsData from '@/data/recommendations.json';
 
 const Recommendations = () => {
   return (
     <section className="py-16" id="recommendations">
       <Container>
-        <h2 className="text-center text-xl">Recommendations</h2>
+        <h2 className="text-center text-xl">LinkedIn Recommendations</h2>
 
-        <TestimonialBlock
-          imageFileName="testimonials.png"
-          name="Testimonials"
-          about="Testimonials"
-          content="Testimonials"
-        />
+        {recommendationsData.map((item) => (
+          <TestimonialBlock
+            key={item.name}
+            imageFileName={item.imageFileName}
+            name={item.name}
+            about={item.about}
+            content={item.content}
+          />
+        ))}
       </Container>
     </section>
   );
