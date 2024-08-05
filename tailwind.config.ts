c7f1fdc436a9caf2f55bb1429fb7175158b59e2e
nzkks <kkshanthosh@gmail.com>
2024-08-06 11:26:14 +1200

    minor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,6 +75,7 @@ const config: Config = {
         },
       },
       borderRadius: {
+        xl: `calc(var(--radius) + 4px)`,
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
