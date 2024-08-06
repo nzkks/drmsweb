5dd41cc24defc7838e661aa01aa3c42fc5f8fb56
nzkks <kkshanthosh@gmail.com>
2024-08-06 16:20:28 +1200

    Homepage - Certifications: Link the block with the external url

--- src/components/common/certificationBlock/certification-block.tsx
+++ src/components/common/certificationBlock/certification-block.tsx
@@ -1,5 +1,6 @@
-import { ExternalLinkIcon } from '@radix-ui/react-icons';
 import Image from 'next/image';
+import Link from 'next/link';
+import { ExternalLinkIcon } from '@radix-ui/react-icons';
 
 type Props = {
   name: string;
@@ -28,9 +29,13 @@ const CertificationBlock = ({
       <div className="absolute inset-0 mx-auto size-full border border-slate-50 p-4 dark:border-slate-800">
         <div className="text-center font-bold">{name}</div>
         <div className="text-center text-sm text-slate-500">{period}</div>
-        <div className="absolute inset-0 m-auto hidden size-8 rounded-lg group-hover:block">
-          <ExternalLinkIcon />
-        </div>
+        <Link
+          href={link}
+          target="_blank"
+          className="relative m-auto hidden size-12 rounded-lg bg-[rgba(204,204,204,0.53)] group-hover:block"
+        >
+          <ExternalLinkIcon className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+        </Link>
         <div className="absolute bottom-[10px] right-0 h-6 w-[200px]">
           {providerLogoFilenameDarkMode ? (
             <>
