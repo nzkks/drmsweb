8b7e3454e46aa289135a1a7d7f475896b9482345
nzkks <kkshanthosh@gmail.com>
2024-08-09 16:51:12 +1200

    Homepage - Certifications: Use Card component

--- src/components/common/certificationBlock/certification-block.tsx
+++ src/components/common/certificationBlock/certification-block.tsx
@@ -1,6 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { ExternalLinkIcon } from '@radix-ui/react-icons';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 type Props = {
   name: string;
@@ -22,62 +30,63 @@ const CertificationBlock = ({
   link,
 }: Props) => {
   return (
-    <div
+    <Card
       key={`${name}-${period}`}
-      className="group relative bg-[rgba(222,222,222,0.21)] transition-all before:block before:pb-[100%] before:content-[''] hover:bg-[rgba(0,0,0,0.05)] dark:bg-[rgba(77,77,77,0.09)] dark:hover:bg-[rgba(111,111,111,0.1)]"
+      className="group flex flex-col border-border bg-blue-500/10 bg-clip-padding saturate-100 backdrop-blur backdrop-contrast-100"
     >
-      <Link
-        href={link}
-        target="_blank"
-        className="absolute left-1/2 top-1/2 hidden size-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-[rgba(204,204,204,0.53)] group-hover:flex"
-      >
-        <ExternalLinkIcon />
-      </Link>
-      <div className="absolute inset-0 mx-auto size-full p-4">
-        <div className="text-center font-bold">{name}</div>
-        <div className="text-center text-sm text-slate-500">{period}</div>
-
-        <div className="absolute bottom-[10px] right-0 h-6 w-[200px]">
-          {providerLogoFilenameDarkMode ? (
-            <>
-              {
-                <>
-                  <div className="absolute hidden size-full dark:block">
-                    <Image
-                      src={`/images/certifications/${providerLogoFilenameDarkMode}`}
-                      fill
-                      style={{
-                        objectFit: 'contain',
-                      }}
-                      alt={providerName}
-                    />
-                  </div>
-                  <div className="absolute size-full dark:hidden">
-                    <Image
-                      src={`/images/certifications/${providerLogoFilename}`}
-                      fill
-                      style={{
-                        objectFit: 'contain',
-                      }}
-                      alt={providerName}
-                    />
-                  </div>
-                </>
-              }
-            </>
-          ) : (
-            <Image
-              src={`/images/certifications/${providerLogoFilename}`}
-              fill
-              style={{
-                objectFit: 'contain',
-              }}
-              alt={providerName}
-            />
-          )}
-        </div>
+      <div className="flex h-full flex-col">
+        <CardHeader className="p-3">
+          <CardTitle className="text-md">{name}</CardTitle>
+          <CardDescription>{period}</CardDescription>
+        </CardHeader>
+        <CardContent className="relative flex grow p-3">
+          <Link
+            href={link}
+            target="_blank"
+            className="absolute left-1/2 top-1/2 hidden size-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-[rgba(204,204,204,0.53)] group-hover:flex"
+          >
+            <ExternalLinkIcon />
+          </Link>
+        </CardContent>
+        <CardFooter className="p-3">
+          <div className="relative h-6 w-full">
+            {providerLogoFilenameDarkMode ? (
+              <>
+                <div className="absolute hidden size-full dark:block">
+                  <Image
+                    src={`/images/certifications/${providerLogoFilenameDarkMode}`}
+                    fill
+                    style={{
+                      objectFit: 'contain',
+                    }}
+                    alt={providerName}
+                  />
+                </div>
+                <div className="absolute size-full dark:hidden">
+                  <Image
+                    src={`/images/certifications/${providerLogoFilename}`}
+                    fill
+                    style={{
+                      objectFit: 'contain',
+                    }}
+                    alt={providerName}
+                  />
+                </div>
+              </>
+            ) : (
+              <Image
+                src={`/images/certifications/${providerLogoFilename}`}
+                fill
+                style={{
+                  objectFit: 'contain',
+                }}
+                alt={providerName}
+              />
+            )}
+          </div>
+        </CardFooter>
       </div>
-    </div>
+    </Card>
   );
 };
 
