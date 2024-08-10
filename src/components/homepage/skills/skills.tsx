4b81fcb76933cba5996a4bf3ab340c67b8d2dae7
nzkks <kkshanthosh@gmail.com>
2024-08-10 22:20:57 +1200

    Homepage - Skills: show a dropdown menu

--- src/components/homepage/skills/skills.tsx
+++ src/components/homepage/skills/skills.tsx
@@ -1,10 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@nextui-org/react';
+import { DotsVerticalIcon } from '@radix-ui/react-icons';
 import Marquee from 'react-fast-marquee';
 
 import { Container } from '@/components';
 import skillsData from '@/data/skills.json';
 import groupObjArrayByProperty from '@/lib/groupObjArrayByProperty';
 
+const menuItems = [
+  {
+    key: 'experience',
+    label: 'Show experience level',
+  },
+  {
+    key: 'category',
+    label: 'Show categorized',
+  },
+  {
+    key: 'animation',
+    label: 'Show scroll animation',
+  },
+];
+
 const Skills = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState('animation');
+
   const groupedSkillData = groupObjArrayByProperty(
     skillsData,
     (x) => x.category,
@@ -34,10 +62,45 @@ const Skills = () => {
 
   return (
     <section className="py-16" id="skills">
-      <h2 className="text-center text-xl">
-        I turn these skills into beautiful web projects
-      </h2>
-      <Container>{marquees}</Container>
+      <div className="flex items-center justify-center">
+        <div>
+          <h2 className="text-center text-xl">
+            I turn these skills into beautiful web projects
+          </h2>
+        </div>
+        <div className="ml-2 w-12">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button isIconOnly variant="flat" className="size-8">
+                <DotsVerticalIcon />
+              </Button>
+            </DropdownTrigger>
+
+            <DropdownMenu aria-label="Static Actions">
+              {menuItems.map((item) => (
+                <DropdownItem
+                  key={item.key}
+                  onPress={() => setSelectedMenuItem(item.key)}
+                >
+                  {item.label}
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+
+      {selectedMenuItem === 'animation' ? (
+        <Container>{marquees}</Container>
+      ) : selectedMenuItem === 'experience' ? (
+        <Container>
+          <>Experience</>
+        </Container>
+      ) : (
+        <Container>
+          <>Categorized</>
+        </Container>
+      )}
     </section>
   );
 };
