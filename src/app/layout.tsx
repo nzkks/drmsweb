9cc0d8b991b58a89cbf2f7f52fffb6a85027a7bb
nzkks <kkshanthosh@gmail.com>
2024-08-10 12:18:02 +1200

    change the min height of the body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ const RootLayout = ({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans text-foreground antialiased',
+          'min-h-svh bg-background font-sans text-foreground antialiased',
           fontSans.variable,
         )}
       >
