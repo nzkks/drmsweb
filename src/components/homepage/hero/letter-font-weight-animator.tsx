ec0ff9d2380576fb0d456289d951e09cd3fa416b
nzkks <kkshanthosh@gmail.com>
2024-08-13 22:32:59 +1200

    Homepage - Hero: Add smooth font weight based animation to a word

--- src/components/homepage/hero/letter-font-weight-animator.tsx
+++ src/components/homepage/hero/letter-font-weight-animator.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from 'react';
 import { cn } from '@/lib/utils';
 
 type Props = {
@@ -6,16 +9,58 @@ type Props = {
 };
 
 const LetterFontWeightAnimator = ({ word, className }: Props) => {
+  useEffect(() => {
+    const spans = document.querySelectorAll(
+      '.hover-text span',
+    ) as NodeListOf<HTMLSpanElement>;
+
+    spans.forEach((span) => {
+      span.addEventListener('mouseenter', function (this: typeof span) {
+        this.style.fontWeight = '900';
+        this.style.color = 'hsl(var(--accent))';
+
+        const leftNeighbor = this.previousElementSibling as HTMLSpanElement;
+        const rightNeighbor = this.nextElementSibling as HTMLSpanElement;
+
+        if (leftNeighbor) {
+          leftNeighbor.style.fontWeight = '700';
+          leftNeighbor.style.color = 'hsla(var(--accent) / 0.4)';
+        }
+        if (rightNeighbor) {
+          rightNeighbor.style.fontWeight = '700';
+          rightNeighbor.style.color = 'hsla(var(--accent) / 0.4)';
+        }
+      });
+
+      span.addEventListener('mouseleave', function (this: typeof span) {
+        this.style.fontWeight = '400';
+        this.style.color = 'hsl(var(--foreground))';
+
+        const leftNeighbor = this.previousElementSibling as HTMLSpanElement;
+        const rightNeighbor = this.nextElementSibling as HTMLSpanElement;
+
+        if (leftNeighbor) {
+          leftNeighbor.style.fontWeight = '400';
+          leftNeighbor.style.color = 'hsl(var(--foreground))';
+        }
+
+        if (rightNeighbor) {
+          rightNeighbor.style.fontWeight = '400';
+          rightNeighbor.style.color = 'hsl(var(--foreground))';
+        }
+      });
+    });
+  }, []);
+
   const renderedWord = word.split('').map((letter, index) => (
-    <span
-      key={index}
-      className="font-thin transition-all hover:font-black hover:text-accent"
-    >
+    <span key={index} style={{ transition: '0.35s font-weight, 0.35s color' }}>
       {letter}
     </span>
   ));
 
-  return <div className={cn('w-[275px]', className)}>{renderedWord}</div>;
+  return (
+    <div className={cn('hover-text w-[275px]', className)}>{renderedWord}</div>
+  );
 };
 
 export default LetterFontWeightAnimator;
