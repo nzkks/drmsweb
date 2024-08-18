ca0a1eecfa66026cdeb4da5f5f4e93ec7b758035
nzkks <kkshanthosh@gmail.com>
2024-08-18 14:11:15 +1200

    Homepage - Skills - Marquee: replace each skill wrapper with ChipWrapper component

--- src/components/homepage/skills/skills-marquee.tsx
+++ src/components/homepage/skills/skills-marquee.tsx
@@ -2,6 +2,7 @@ import Marquee from 'react-fast-marquee';
 
 import groupObjArrayByProperty from '@/lib/groupObjArrayByProperty';
 import { Skill } from '@/types';
+import { ChipWrapper } from '@/components/common';
 
 type Props = {
   data: Skill[];
@@ -16,20 +17,20 @@ const SkillsMarquee = ({ data }: Props) => {
       direction={index % 2 === 0 ? 'left' : 'right'}
       speed={20}
       autoFill
-      className="my-8"
+      className="my-6"
     >
       {skillRowsForMarquee[rowId].map((skill) => {
         return (
-          <div
+          <ChipWrapper
             key={skill.name}
-            className="block-glass1 mx-4 rounded-xl border-1 px-3 py-2 shadow dark:border-[#2f4250]"
+            className="block-glass1 mx-2 rounded-xl border-1 px-1 py-2 shadow dark:border-[#2f4250]"
           >
             {skill.isHighlight ? (
               <span className="font-bold text-accent">{skill.name}</span>
             ) : (
               <span>{skill.name}</span>
             )}
-          </div>
+          </ChipWrapper>
         );
       })}
     </Marquee>
