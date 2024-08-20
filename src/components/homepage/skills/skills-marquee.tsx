5c0d1efb50d225307df547f1c9f2b5da73056957
nzkks <kkshanthosh@gmail.com>
2024-08-20 20:29:57 +1200

    Homepage - Skills marquee: Show skeleton while the skills data getting ready for mapping

--- src/components/homepage/skills/skills-marquee.tsx
+++ src/components/homepage/skills/skills-marquee.tsx
@@ -1,4 +1,4 @@
-import { Chip } from '@nextui-org/react';
+import { Chip, Skeleton } from '@nextui-org/react';
 import Marquee from 'react-fast-marquee';
 
 import groupObjArrayByProperty from '@/lib/groupObjArrayByProperty';
@@ -11,30 +11,34 @@ type Props = {
 const SkillsMarquee = ({ data }: Props) => {
   const skillRowsForMarquee = groupObjArrayByProperty(data, (x) => x.rowId);
 
-  return Object.keys(skillRowsForMarquee).map((rowId, index) => (
-    <Marquee
-      key={rowId}
-      direction={index % 2 === 0 ? 'left' : 'right'}
-      speed={20}
-      autoFill
-      className="my-6"
-    >
-      {skillRowsForMarquee[rowId].map((skill) => {
-        return (
-          <Chip
-            key={skill.name}
-            className="block-glass1 shadow-3xl mx-2 border-1 dark:border-[#2f4250]"
-          >
-            {skill.isHighlight ? (
-              <span className="font-bold text-accent">{skill.name}</span>
-            ) : (
-              <span>{skill.name}</span>
-            )}
-          </Chip>
-        );
-      })}
-    </Marquee>
-  ));
+  return (
+    <Skeleton isLoaded={!!skillRowsForMarquee} className="h-48 w-full">
+      {Object.keys(skillRowsForMarquee).map((rowId, index) => (
+        <Marquee
+          key={rowId}
+          direction={index % 2 === 0 ? 'left' : 'right'}
+          speed={20}
+          autoFill
+          className="my-6"
+        >
+          {skillRowsForMarquee[rowId].map((skill) => {
+            return (
+              <Chip
+                key={skill.name}
+                className="block-glass1 shadow-3xl mx-2 border-1 dark:border-[#2f4250]"
+              >
+                {skill.isHighlight ? (
+                  <span className="font-bold text-accent">{skill.name}</span>
+                ) : (
+                  <span>{skill.name}</span>
+                )}
+              </Chip>
+            );
+          })}
+        </Marquee>
+      ))}
+    </Skeleton>
+  );
 };
 
 export default SkillsMarquee;
