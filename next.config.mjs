5fb4a518aa203e64a86f974d749c856b8371ecc8
nzkks <kkshanthosh@gmail.com>
2024-08-21 23:12:55 +1200

    Update GitHub workflow files and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/drmsweb', // <=== this is specific to hosting in GitHub Pages. Because the site is deployed as https://nzkks.github.io/drmsweb/.So there is a subpath in the URL. This line will make the static assets works in GitHub Pages.
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
