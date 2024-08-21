9301f6fedac84fbbb9603ae45a6df49c41beec19
nzkks <kkshanthosh@gmail.com>
2024-08-21 23:37:06 +1200

    GitHub workflow - publish.yml: Change the configure-pages version

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+const nextConfig: NextConfig = {
   basePath: '/drmsweb', // <=== this is specific to hosting in GitHub Pages. Because the site is deployed as https://nzkks.github.io/drmsweb/.So there is a subpath in the URL. This line will make the static assets works in GitHub Pages.
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
