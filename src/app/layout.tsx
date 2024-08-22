210d1df5ec79d5ca90671e3fd3ca3b974c17fa05
nzkks <kkshanthosh@gmail.com>
2024-08-22 20:00:19 +1200

    Layout: Add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,47 @@ const fontSans = FontSans({
   variable: '--font-sans',
 });
 
+const titleText =
+  'Shanthosh Krishnakumar | Frontend Developer | React, Next.js, Animations | Auckland';
+const descriptionText =
+  'Discover innovative frontend development solutions with cutting-edge technologies. Specializing in responsive designs, intuitive user interfaces, and optimized performance.';
+
 export const metadata: Metadata = {
-  title:
-    'Shanthosh Krishnakumar | Frontend Developer | React, Next.js, Animations | Auckland',
-  description:
-    'Discover innovative frontend development solutions with cutting-edge technologies. Specializing in responsive designs, intuitive user interfaces, and optimized performance.',
+  title: titleText,
+  description: descriptionText,
+  alternates: {
+    canonical: 'https://drmsweb.com',
+  },
+  keywords: [
+    'frontend',
+    'developer',
+    'website',
+    'web',
+    'auckland',
+    'shanthosh',
+    'krishnakumar',
+    'reactjs',
+    'nextjs',
+    'development',
+  ],
+  creator: 'Shanthosh Krishnakumar',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    url: 'https://drmsweb.com',
+    title: titleText,
+    description: descriptionText,
+    siteName: 'Shanthosh - Frontend Developer',
+    // images: [
+    //   {
+    //     url: 'https://drmsweb.com/cover.png',
+    //     width: 1200,
+    //     height: 630,
+    //     alt: titleText,
+    //   },
+    // ],
+  },
+  category: 'Web Development',
 };
 
 const RootLayout = ({
