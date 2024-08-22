1bb72f75ea077ca9f3932f8209cb3721ec4916d2
nzkks <kkshanthosh@gmail.com>
2024-08-22 19:38:19 +1200

    next.config.mjs: minor cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Since the site is having a proper domain name instead of GitHub's one, the below lines are not needed.
-  // basePath: process.env.NODE_ENV === 'production' ? '/drmsweb' : '',
-  // assetPrefix:
-  //   process.env.NODE_ENV === 'production'
-  //     ? 'https://nzkks.github.io/drmsweb'
-  //     : '',
   output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
   reactStrictMode: true,
 };
