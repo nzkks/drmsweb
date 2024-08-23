98ef0f21ea0914fdd5d5e41392f98e5decf335f4
nzkks <kkshanthosh@gmail.com>
2024-08-23 16:56:55 +1200

    Analytics: try fixing GA and GTM tags showing up in the website

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -1,14 +1,13 @@
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
+const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string;
+const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string;
+
 const Analytics = () => {
   return process.env.NODE_ENV === 'production' ? (
     <>
-      <GoogleAnalytics
-        gaId={String(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID)}
-      />
-      <GoogleTagManager
-        gtmId={String(process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID)}
-      />
+      <GoogleAnalytics gaId={GA_ID} />
+      <GoogleTagManager gtmId={GTM_ID} />
     </>
   ) : null;
 };
