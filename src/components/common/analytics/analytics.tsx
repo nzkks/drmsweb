65f57e914cd3c387ec36f5870c26e3da5eff8a65
nzkks <kkshanthosh@gmail.com>
2024-08-23 12:00:43 +1200

    Use the Google Analytics tag/component from the @next/third-parties/google library

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -1,25 +1,12 @@
 import conf from '@/lib/config';
-import { GoogleTagManager } from '@next/third-parties/google';
-import Script from 'next/script';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const Analytics = () => {
   const isProd = conf.environment === 'production';
 
   return !isProd ? null : (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${conf.googleAnalyticsId}`}
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-js">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${conf.googleAnalyticsId}');
-          `}
-      </Script>
+      <GoogleAnalytics gaId={conf.googleAnalyticsId} />
       <GoogleTagManager gtmId={conf.googleTagManagerId} />
     </>
   );
