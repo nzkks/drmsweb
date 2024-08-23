72667f4040ce71b16415426e7dbc429f7a58a037
nzkks <kkshanthosh@gmail.com>
2024-08-23 13:01:26 +1200

    Fix loading Analytics script only on production site

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -2,9 +2,7 @@ import conf from '@/lib/config';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const Analytics = () => {
-  const isProd = conf.environment === 'production';
-
-  return isProd ? (
+  return process.env.NODE_ENV === 'production' ? (
     <>
       <GoogleAnalytics gaId={conf.googleAnalyticsId} />
       <GoogleTagManager gtmId={conf.googleTagManagerId} />
