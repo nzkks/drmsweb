d4fa9605a2acfb37c84ab9e186ef59c8e64343dc
nzkks <kkshanthosh@gmail.com>
2024-08-23 13:21:24 +1200

    Analytics: finally fix loading the environment variables

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -1,11 +1,14 @@
-import conf from '@/lib/config';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const Analytics = () => {
   return process.env.NODE_ENV === 'production' ? (
     <>
-      <GoogleAnalytics gaId={conf.googleAnalyticsId} />
-      <GoogleTagManager gtmId={conf.googleTagManagerId} />
+      <GoogleAnalytics
+        gaId={String(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID)}
+      />
+      <GoogleTagManager
+        gtmId={String(process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID)}
+      />
     </>
   ) : null;
 };
--- src/lib/config.ts
+++
@@ -1,6 +0,0 @@
-const conf = {
-  googleAnalyticsId: String(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID),
-  googleTagManagerId: String(process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID),
-};
-
-export default conf;
