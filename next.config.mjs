b468005f57f8e69ce94e8d657dc3c0f8d5d9e30f
nzkks <kkshanthosh@gmail.com>
2024-08-26 21:22:23 +1200

    minor

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+  },
 };
 
 export default nextConfig;
