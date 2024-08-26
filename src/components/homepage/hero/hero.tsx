f53dd2cbb6a33f1138d463c8a8123cbf2524a049
nzkks <kkshanthosh@gmail.com>
2024-08-27 11:39:37 +1200

    Homepage - Hero: Add the buttons which were in CTA section

--- src/components/homepage/hero/hero.tsx
+++ src/components/homepage/hero/hero.tsx
@@ -3,6 +3,9 @@ import { Chip } from '@nextui-org/react';
 import { ContactIcons, Container, ShinyBtn } from '@/components';
 import skillsData from '@/data/skills.json';
 import LetterFontWeightAnimator from './letter-font-weight-animator';
+import { PiFilePdfBold } from 'react-icons/pi';
+import { TbFileTypeDocx } from 'react-icons/tb';
+import { MdOutlineAlternateEmail } from 'react-icons/md';
 
 const Hero = () => {
   return (
@@ -28,16 +31,45 @@ const Hero = () => {
             Frontend Developer
           </span>
         </h1>
-        <div className="mt-6 text-center">
-          <ShinyBtn
-            href="#cta"
-            width={144}
-            className="mx-auto"
-            buttonIdForGA="Hero-HireMe-Link-Clicked"
-            valueForGA="Hire me"
-          >
-            Hire me
-          </ShinyBtn>
+        <div className="mt-10 text-center">
+          <div className="mx-auto mt-10 grid max-w-xl items-center justify-center gap-4 auto-fit-[170px]">
+            <ShinyBtn
+              href="/files/ShanthoshK_Frontend_Dev_cv.pdf"
+              target="_blank"
+              isExternal={true}
+              downloadFileName="ShanthoshK_Frontend_Dev_cv.pdf"
+              aria-label="download cv"
+              width={180}
+              buttonIdForGA="CTA-CV-PDF-Download-Link-Clicked"
+              valueForGA="Download PDF cv"
+            >
+              <PiFilePdfBold className="size-8" />
+              <div className="ml-2 inline-block">Download cv</div>
+            </ShinyBtn>
+            <ShinyBtn
+              href="/files/ShanthoshK_Frontend_Dev_cv.docx"
+              target="_blank"
+              isExternal={true}
+              downloadFileName="ShanthoshK_Frontend_Dev_cv.docx"
+              aria-label="download cv"
+              width={180}
+              buttonIdForGA="CTA-CV-DOCX-Download-Link-Clicked"
+              valueForGA="Download DOCX cv"
+            >
+              <TbFileTypeDocx className="size-8" />
+              <div className="ml-2 inline-block">Download cv</div>
+            </ShinyBtn>
+            <ShinyBtn
+              href="mailto:kkshanthosh@gmail.com"
+              ariaLabel="email me"
+              width={180}
+              buttonIdForGA="CTA-Email-Link-Clicked"
+              valueForGA="Email me"
+            >
+              <MdOutlineAlternateEmail className="size-7" />
+              <div className="ml-2 inline-block">Email me</div>
+            </ShinyBtn>
+          </div>
         </div>
       </Container>
     </section>
