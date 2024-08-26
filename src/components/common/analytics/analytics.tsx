06e6c895199cca2139f209a290ca6733afe39827
nzkks <kkshanthosh@gmail.com>
2024-08-26 20:44:31 +1200

    Since mentioned the home pc ip address in GA, I can remove the GA works only on production environment flag

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -4,12 +4,12 @@ const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string;
 const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string;
 
 const Analytics = () => {
-  return process.env.NODE_ENV === 'production' ? (
+  return (
     <>
       <GoogleAnalytics gaId={GA_ID} />
       <GoogleTagManager gtmId={GTM_ID} />
     </>
-  ) : null;
+  );
 };
 
 export default Analytics;
