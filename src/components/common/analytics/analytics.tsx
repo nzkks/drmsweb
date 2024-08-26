c428189174e44fc6388a37eb67cb3f0c474388e6
nzkks <kkshanthosh@gmail.com>
2024-08-26 21:43:59 +1200

    minor

--- src/components/common/analytics/analytics.tsx
+++ src/components/common/analytics/analytics.tsx
@@ -1,13 +1,14 @@
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
-const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string;
-const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string;
-
 const Analytics = () => {
   return (
     <>
-      <GoogleAnalytics gaId={GA_ID} />
-      <GoogleTagManager gtmId={GTM_ID} />
+      <GoogleAnalytics
+        gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string}
+      />
+      <GoogleTagManager
+        gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string}
+      />
     </>
   );
 };
