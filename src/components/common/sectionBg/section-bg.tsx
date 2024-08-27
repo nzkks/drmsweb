b92c6e6776393fe8e8729f4146e59df20add5c67
nzkks <kkshanthosh@gmail.com>
2024-08-27 20:25:51 +1200

    Section bg: show dark/light theme based image

--- src/components/common/sectionBg/section-bg.tsx
+++ src/components/common/sectionBg/section-bg.tsx
@@ -1,14 +1,26 @@
+'use client';
+
 import Image from 'next/image';
-import sectionImg from '../../../../public/images/bg-images/section.svg';
+import { useTheme } from 'next-themes';
+
+import sectionBgImg from '../../../../public/images/bg-images/section-bg.svg';
+import sectionBgImgDark from '../../../../public/images/bg-images/section-bg--dark.svg';
 
 type Props = {
   top?: string;
 };
 
 const SectionBg = ({ top = '0px' }: Props) => {
+  const { theme } = useTheme();
+
+  let imageUrl = sectionBgImgDark;
+  if (theme === 'light') {
+    imageUrl = sectionBgImg;
+  }
+
   return (
     <Image
-      src={sectionImg}
+      src={imageUrl}
       alt="section background image"
       width={1572}
       height={795}
