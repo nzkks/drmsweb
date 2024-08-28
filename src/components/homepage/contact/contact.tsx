53f055b868fa49f095b2add2fee35e96db6190dc
nzkks <kkshanthosh@gmail.com>
2024-08-28 23:45:03 +1200

    Homepage - Contact: Add all other contact details

--- src/components/homepage/contact/contact.tsx
+++ src/components/homepage/contact/contact.tsx
@@ -1,4 +1,8 @@
-import { Container, SectionBg } from '@/components/common';
+import Link from 'next/link';
+import { MdOutlineAlternateEmail } from 'react-icons/md';
+import { MdLocalPhone } from 'react-icons/md';
+
+import { ContactIcons, Container, SectionBg } from '@/components/common';
 import ContactForm from './contact-form';
 
 const Contact = () => {
@@ -8,8 +12,36 @@ const Contact = () => {
       <Container className="py-16">
         <h2 className="text-center text-xl">Contact</h2>
 
-        <div className="mt-6">
-          <div className="mx-auto max-w-xl">
+        <div className="mx-auto mt-6 grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <div>
+            <div className="mb-2 font-semibold">Connect with me:</div>
+            <ContactIcons align="start" />
+            <div className="mt-6 flex font-semibold">
+              <MdOutlineAlternateEmail className="size-6" />{' '}
+              <span className="ml-1">Email:</span>
+            </div>
+            <div>
+              <Link
+                href="mailto:kkshanthosh@gmail.com"
+                className="text-accent underline hover:text-heading"
+              >
+                kkshanthosh@gmail.com
+              </Link>
+            </div>
+            <div className="mt-6 flex font-semibold">
+              <MdLocalPhone className="size-5" />
+              <span className="ml-1">Phone:</span>
+            </div>
+            <div>
+              <Link
+                href="tel:+642102495970"
+                className="text-accent underline hover:text-heading"
+              >
+                +64 21 024 95970
+              </Link>
+            </div>
+          </div>
+          <div className="mt-12 lg:mt-0">
             <ContactForm />
           </div>
         </div>
