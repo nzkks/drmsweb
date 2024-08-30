fcfada6e344b4176de48edd5423e2595cf8b49b4
nzkks <kkshanthosh@gmail.com>
2024-08-30 18:07:15 +1200

    Theme Switch: Remove the dropdown options and just on click swap the light/dark theme

--- src/components/common/themeSwitch/theme-switch.tsx
+++ src/components/common/themeSwitch/theme-switch.tsx
@@ -1,63 +1,26 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-import {
-  Button,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-} from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
-
-import { MoonIcon } from '@/components';
-const SunIconComponent = dynamic(
-  () => import('@/components/imageComponents/sunIcon'),
-  { ssr: false },
-);
-
-const themeVariants = [
-  // {
-  //   key: 'system',
-  //   label: 'System',
-  // },
-  {
-    key: 'light',
-    label: 'Light',
-  },
-  {
-    key: 'dark',
-    label: 'Dark',
-  },
-];
+import { RxMoon as DarkModeIcon } from 'react-icons/rx';
+import { RxSun as LightModeIcon } from 'react-icons/rx';
 
 const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button isIconOnly variant="light" aria-label="theme mode">
-          <span className="hidden dark:inline-block">
-            <MoonIcon isActive={theme === 'dark'} />
-          </span>
-          <span className="dark:hidden">
-            <SunIconComponent isActive={theme === 'light'} />
-          </span>
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu items={themeVariants}>
-        {(item) => (
-          <DropdownItem
-            key={item.key}
-            onPress={() => setTheme(item.key)}
-            color="default"
-          >
-            {item.label}
-          </DropdownItem>
-        )}
-      </DropdownMenu>
-    </Dropdown>
+    <Button
+      isIconOnly
+      variant="light"
+      aria-label="theme mode"
+      onPress={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'dark' ? (
+        <DarkModeIcon size={16} />
+      ) : (
+        <LightModeIcon size={16} />
+      )}
+    </Button>
   );
 };
 
