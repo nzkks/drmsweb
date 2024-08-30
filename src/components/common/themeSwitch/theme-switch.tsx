cf8d7b64406261764be17c69ac1daa990e9d381c
nzkks <kkshanthosh@gmail.com>
2024-08-30 20:30:19 +1200

    Theme Switch: fix the 'prop `d`' did not match. Server: ... Client:...' issue

--- src/components/common/themeSwitch/theme-switch.tsx
+++ src/components/common/themeSwitch/theme-switch.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { Button } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
 import { RxMoon as DarkModeIcon } from 'react-icons/rx';
-import { RxSun as LightModeIcon } from 'react-icons/rx';
+import { HiSun as LightModeIcon } from 'react-icons/hi';
 
 const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, [setLoaded]);
 
   return (
     <Button
@@ -16,10 +21,18 @@ const ThemeSwitch = () => {
       onPress={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       className="group"
     >
-      {theme === 'dark' ? (
-        <LightModeIcon size={16} className="group-hover:animate-spin" />
+      {loaded ? (
+        theme === 'dark' ? (
+          <span className="group-hover:animate-spin">
+            <LightModeIcon size={24} />
+          </span>
+        ) : (
+          <span className="group-hover:animate-ping">
+            <DarkModeIcon size={20} />
+          </span>
+        )
       ) : (
-        <DarkModeIcon size={16} className="group-hover:animate-ping" />
+        <span className="animate-pulse">...</span>
       )}
     </Button>
   );
