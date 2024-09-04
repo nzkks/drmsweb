1baa0fcf7fb60c85375db4f7277e191aa2cf72f4
nzkks <kkshanthosh@gmail.com>
2024-09-04 15:52:43 +1200

    Homepage - About section: remove unnecesssary collapsible functionality

--- src/components/common/collapsibleWrapper/collapsible-wrapper.tsx
+++ src/components/common/collapsibleWrapper/collapsible-wrapper.tsx
@@ -1,54 +1,18 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { BsArrowsExpand, BsX } from 'react-icons/bs';
-import { sendGTMEvent } from '@next/third-parties/google';
-
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/components/ui/collapsible';
-
 type Props = {
   title: string;
   content: string;
 };
 
 const CollapsibleWrapper = ({ title, content }: Props) => {
-  const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    if (open) {
-      sendGTMEvent({
-        event: 'About-Info-Viewed',
-        value: title,
-      });
-    }
-  }, [open, title]);
-
   return (
-    <Collapsible open={open} onOpenChange={setOpen} asChild>
-      <>
-        <CollapsibleTrigger>
-          <div className="mb-2 flex items-center">
-            <div className="IconButton flex size-6 items-center justify-center">
-              {open ? (
-                <BsX size={24} aria-hidden={true} />
-              ) : (
-                <BsArrowsExpand size={16} aria-hidden={true} />
-              )}
-            </div>
-            <div className="ml-3 font-bold transition-all group-hover/inner:text-accent">
-              {title}
-            </div>
-          </div>
-        </CollapsibleTrigger>
-        <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-slide-up data-[state=open]:animate-slide-down">
-          {content}
-        </CollapsibleContent>
-      </>
-    </Collapsible>
+    <>
+      <div className="mb-2 flex items-center">
+        <div className="font-bold transition-all group-hover/inner:text-accent">
+          {title}
+        </div>
+      </div>
+      <div>{content}</div>
+    </>
   );
 };
 
